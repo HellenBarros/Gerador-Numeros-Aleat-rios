667875895609c972f7a22998f74e188dcf1e3105
HellenBarros <hellenbarrosac@gmail.com>
2022-06-29 00:15:49 -0300

    commit inicial

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import{ SafeAreaView, 
+        TouchableOpacity,
+        StyleSheet,
+        Text} from 'react-native';
+//safeareaview local seguro para aparecer componentes na tela
+const App = () =>{
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [numero, setNumero] = useState('0');
+
+  function handleNumero(){
+    const novo_numero = Math.floor(Math.random() * 10);
+    setNumero(novo_numero)
+  }
+
+  return(
+    <SafeAreaView style={style.container}>
+      <Text style={style.numero}>{numero}</Text>
+      <TouchableOpacity onPress={handleNumero}  style={style.botao}>
+        <Text>Gerar Número</Text>
+      </TouchableOpacity>
+
+    </SafeAreaView>
   );
-}
-
-const styles = StyleSheet.create({
+};
+const style = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'red',
+    flex:1, //Tela toda
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  numero:{
+    fontSize: 38,
+    color:'white',
+    fontWeight: 'bold',
+  },
+  botao:{
+    backgroundColor:'white',
+    width:'80%', //tamanho da linha branca
+    paddingHorizontal: 5,
+    paddingVertical: 10, //largura da linha
+    borderRadius: 5, //bordas arredondadas 
     justifyContent: 'center',
+    alignItems:'center',
+    marginTop:10, //afastar do numeo
   },
 });
+export default App;
